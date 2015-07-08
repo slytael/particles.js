7fc841bb8e8d7ce3a492aa8acbab29bd86b8b09c
Chris Paynter <chrispaynter@me.com>
2015-07-08 20:27:22 +0100

    Added disclaimer to top of file

--- particles.js
+++ particles.js
@@ -1,11 +1,14 @@
+/* THIS HAS BEEN MODIFIED FROM THE ORIGINAL SOURCE.
+/* SEE THE DEEPEND GITHUB
 /* -----------------------------------------------
 /* Author : Vincent Garreau  - vincentgarreau.com
 /* MIT license: http://opensource.org/licenses/MIT
 /* Demo / Generator : vincentgarreau.com/particles.js
-/* GitHub : github.com/VincentGarreau/particles.js
+/* GitHub : https://github.com/deepend-melbourne/particles.js
 /* How to use? : Check the GitHub README
 /* v2.0.0
 /* ----------------------------------------------- */
+
 
 var pJS = function(tag_id, params){
 
